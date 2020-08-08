9b0f39cbfdceb09c41b7fc94dc5132a8ab43152d
Nicolle <nicollepf@gmail.com>
2020-08-07 22:50:41 -0300

    main class

--- strings-race/src/main/java/com/nicollefavero/stringsrace/StringsRaceApp.java
+++ strings-race/src/main/java/com/nicollefavero/stringsrace/StringsRaceApp.java
@@ -2,20 +2,32 @@ package com.nicollefavero.stringsrace;
 
 public class StringsRaceApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        StringRacer stringRacer = new StringRacer();
+        StringBuilderRacer stringBuilderRacer = new StringBuilderRacer();
+        StringBufferRacer stringBufferRacer = new StringBufferRacer();
 
-        String finalString = "";
-        long tStart = System.currentTimeMillis();
+        long stringRacerTime = stringRacer.concatenator();
+        long stringBuilderRacerTime = stringBuilderRacer.concatenator();
+        long stringBufferRacerTime = stringBufferRacer.concatenator();
 
-        for(int i = 0; i < 100000; i++){
-            finalString += "a";
+        System.out.println("Tempo de concatenação com String (+): " + stringRacerTime);
+        System.out.println("Tempo de concatenação com String Builder: " + stringBuilderRacerTime);
+        System.out.println("Tempo de concatenação com String Buffer: " + stringBufferRacerTime);
+
+        System.out.println("\nAnd the winner is....");
+
+        Thread.sleep(5000);
+
+        if(stringBuilderRacerTime < stringRacerTime){
+            if(stringBuilderRacerTime < stringBufferRacerTime){
+                System.out.print("STRING BUILDER RACER!!");
+            } else {
+                System.out.println("STRING BUFFER RACER!!");
+            }
+        } else {
+            System.out.println("STRING RACER WITH +++++!!");
         }
-
-        long tEnd = System.currentTimeMillis();
-        long tFinal = tEnd - tStart;
-
-        System.out.println("");
-
     }
 }
